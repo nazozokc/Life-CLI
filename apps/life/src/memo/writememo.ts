2c55597cf3c0dcb5dc1a69e3980c099b9b0e3e6f
nazozokc <nazozokc@gmail.com>
2026-07-31 17:02:12 +0900

    add memo

--- apps/life/src/memo/writememo.ts
+++ apps/life/src/memo/writememo.ts
@@ -1,4 +1,25 @@
-import { writeFile } from "node:fs/promises";
+import { writeFile, mkdir } from "node:fs/promises";
 import { MEMO_DIR } from "../constant/app.ts";
+import openEditor from "open-editor";
+import { join } from "node:path";
 
-export const writememo = async (): Promise<void> => {};
+export const writememo = async (filename: string): Promise<void> => {
+  await mkdir(MEMO_DIR, { recursive: true });
+  const path = join(MEMO_DIR, `${filename}.md`);
+
+  try {
+    await writeFile(path, "", { flag: "wx" });
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code !== "EEXIST") {
+      throw error;
+    }
+  }
+
+  await openEditor([
+    {
+      file: path,
+      line: 1,
+      column: 1,
+    },
+  ]);
+};
