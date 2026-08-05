53f56d653ba12f2782acc3981c996dc9f190d1a6
nazozokc <nazozokc@gmail.com>
2026-08-05 21:28:21 +0900

    edit

--- apps/life/src/cli/memo/add.ts
+++ apps/life/src/cli/memo/add.ts
@@ -1,4 +1,4 @@
-import { writeMemo } from "../../memo/writememo.ts";
+import { addMemo } from "../../memo/addmemo.ts";
 import { input } from "@inquirer/prompts";
 
 export const add = async (): Promise<void> => {
@@ -6,5 +6,5 @@ export const add = async (): Promise<void> => {
     message: "ファイル名を入力してください",
   });
 
-  await writeMemo(filename);
+  await addMemo(filename);
 };
