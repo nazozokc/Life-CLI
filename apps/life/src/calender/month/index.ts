e1ea823d350c62054f4bf153ddccda6d2cd68079
nazozokc <nazozokc@gmail.com>
2026-08-05 23:34:33 +0900

    edit

--- apps/life/src/calender/month/index.ts
+++ apps/life/src/calender/month/index.ts
@@ -1,3 +1,10 @@
 import calendarize from "calendarize";
+import consola from "consola";
 
-export const render = async (): Promise<void> => {};
+export const render = async (month?: number): Promise<void> => {
+  const today = new Date();
+  const month = month - 1;
+  const render = calendarize(today.getFullYear(), month ?? today.getMonth());
+
+  consola.log(render);
+};
