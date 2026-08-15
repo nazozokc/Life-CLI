24070b8e8c50e65e685a15b3dadcc59d2e30a961
nazozokc <nazozokc@gmail.com>
2026-08-16 00:49:07 +0900

    Rename deleteNote function to deleteDiary

--- apps/life/src/diary/deleteDiary.ts
+++ apps/life/src/diary/deleteDiary.ts
@@ -2,6 +2,6 @@ import { unlink } from "node:fs/promises";
 import { join } from "path";
 import { DIARY_DIR } from "../constant/app.js";
 
-export const deleteNote = async (name: string): Promise<void> => {
+export const deleteDiary = async (name: string): Promise<void> => {
   await unlink(join(DIARY_DIR, `${name}.md`));
 };
