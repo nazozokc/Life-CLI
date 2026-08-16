f74e7ee7e4a2030b486a9715a917b117c29d2054
nazozokc <nazozokc@users.noreply.github.com>
2026-08-16 13:40:14 +0900

    edit

--- apps/life/src/cli/diary/del.ts
+++ apps/life/src/cli/diary/del.ts
@@ -1,10 +1,25 @@
-import { addNote } from "../../note/addnote.ts";
-import { input } from "@inquirer/prompts";
+import { deleteDiary } from "../../diary/deleteDiary.ts";
+import { DIARY_DIR } from "../../constant/app.ts";
+import { join } from "path";
+import { readFile, access } from "node:fs/promises";
+import consola from "consola";
 
-export const add = async (): Promise<void> => {
-  const filename = await input({
-    message: "Enter a file name",
-  });
+export const add = async (day: string): Promise<void> => {
+  const [year, month, date] = day.split("-");
 
-  await addNote(filename);
+  const filename = join(
+    DIARY_DIR,
+    String(year),
+    String(month),
+    `${year}${month}${date}.md`,
+  );
+
+  try {
+    await access(filename);
+  } catch (error) {
+    consola.error("No such file or directory");
+    return;
+  }
+
+  await deleteDiary(filename);
 };
