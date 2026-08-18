1860e26d0a1005951c983a321a4977a68f1c1191
nazozokc <nazozokc@users.noreply.github.com>
2026-08-18 23:29:08 +0900

    edit

--- apps/life/src/cli/diary/del.ts
+++ apps/life/src/cli/diary/del.ts
@@ -3,9 +3,20 @@ import { DIARY_DIR } from "../../constant/app.ts";
 import { join } from "path";
 import { access } from "node:fs/promises";
 import consola from "consola";
+import { input } from "@inquirer/prompts";
 
-export const del = async (day: string): Promise<void> => {
-  const [year, month, date] = day.split("-");
+export const del = async (): Promise<void> => {
+  const year = await input({
+    message: "Enter a fill year (e.g. 2026)",
+  });
+
+  const month = await input({
+    message: "Enter a month (e.g. 08)",
+  });
+
+  const date = await input({
+    message: "Enter a date (e.g. 01)",
+  });
 
   const filename = join(
     DIARY_DIR,
