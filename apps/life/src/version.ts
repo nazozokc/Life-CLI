2bc372e00904add460b552afc2aec84d5499d05c
nazozokc <nazozokc@users.noreply.github.com>
2026-08-19 16:46:31 +0900

    edit

--- apps/life/src/version.ts
+++ apps/life/src/version.ts
@@ -1,10 +1,11 @@
 import { readFile } from "node:fs/promises";
 
-export const version = async (): Promise<number> => {
-  const fileread = await readFile("../package.json", "utf-8");
+export const version = async (): Promise<string> => {
+  const fileread = await readFile(
+    new URL("../package.json", import.meta.url),
+    "utf-8",
+  );
   const parse = JSON.parse(fileread);
 
-  const ver = parse.version;
-
-  return ver;
+  return parse.version;
 };
