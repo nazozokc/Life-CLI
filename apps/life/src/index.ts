b4ba1381e2af7392a3177271e8f4e9be891c7e0d
nazozokc <nazozokc@users.noreply.github.com>
2026-08-19 22:25:43 +0900

    edit

--- apps/life/src/index.ts
+++ apps/life/src/index.ts
@@ -5,14 +5,16 @@ import { taskCommand } from "./cli/task/index.ts";
 import { noteCommand } from "./cli/note/index.ts";
 import { diaryCommand } from "./cli/diary/index.ts";
 import { version } from "./version.ts";
+import { CLI_COMMAND_NAME } from "./constant/app.ts";
 
 const mainCommand = define({
-  name: "life",
-  version: await version(),
+  name: CLI_COMMAND_NAME,
   description: "A CLI for managing your life",
 });
 
 await cli(process.argv.slice(2), mainCommand, {
+  name: CLI_COMMAND_NAME,
+  version: await version(),
   subCommands: {
     task: taskCommand,
     note: noteCommand,
--- apps/life/src/version.ts
+++ apps/life/src/version.ts
@@ -1,11 +1,15 @@
 import { readFile } from "node:fs/promises";
 
+type packagejson = {
+  version: string;
+};
+
 export const version = async (): Promise<string> => {
   const fileread = await readFile(
     new URL("../package.json", import.meta.url),
     "utf-8",
   );
-  const parse = JSON.parse(fileread);
+  const parse: packagejson = JSON.parse(fileread);
 
   return parse.version;
 };
