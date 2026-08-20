d9b97a2d666d87f2aab67ea2f90c39bd1f54265f
nazozokc <nazozokc@users.noreply.github.com>
2026-08-20 13:55:55 +0900

    edit

--- apps/life/src/search/search.ts
+++ apps/life/src/search/search.ts
@@ -4,40 +4,46 @@ import { join } from "path";
 import { consola } from "consola";
 
 export const searchindex = async (search: string): Promise<void> => {
-  const task = await readdir(TASK_DIR, "utf-8");
+  try {
+    const task = await readdir(TASK_DIR, "utf-8");
 
-  for (const task_file of task) {
-    const path = join(TASK_DIR, task_file);
-    const task = await readFile(path, "utf-8");
+    for (const task_file of task) {
+      const path = join(TASK_DIR, task_file);
+      const task = await readFile(path, "utf-8");
 
-    if (task.includes(search)) {
-      consola.log(path);
-      consola.log(task);
-    }
-  }
-
-  const notes = await readdir(NOTE_DIR, "utf-8");
-  for (const note_file of notes) {
-    const path = join(NOTE_DIR, note_file);
-    const content = await readFile(path, "utf-8");
-
-    if (content.includes(search)) {
-      consola.log(path);
-      consola.log(content);
-    }
-  }
-
-  const diary = await readdir(DIARY_DIR, "utf-8");
-  for (const year of diary) {
-    const read = await readdir(join(DIARY_DIR, year));
-    for (const month of read) {
-      const path = join(DIARY_DIR, year, month);
-      const files = await readFile(path, "utf-8");
-
-      if (files.includes(search)) {
+      if (task.includes(search)) {
         consola.log(path);
-        consola.log(files);
+        consola.log(task);
       }
     }
+
+    const notes = await readdir(NOTE_DIR, "utf-8");
+
+    for (const note_file of notes) {
+      const path = join(NOTE_DIR, note_file);
+      const content = await readFile(path, "utf-8");
+
+      if (content.includes(search)) {
+        consola.log(path);
+        consola.log(content);
+      }
+    }
+
+    const diary = await readdir(DIARY_DIR, "utf-8");
+
+    for (const year of diary) {
+      const read = await readdir(join(DIARY_DIR, year));
+      for (const month of read) {
+        const path = join(DIARY_DIR, year, month);
+        const files = await readFile(path, "utf-8");
+
+        if (files.includes(search)) {
+          consola.log(path);
+          consola.log(files);
+        }
+      }
+    }
+  } catch (error) {
+    consola.error(error);
   }
 };
